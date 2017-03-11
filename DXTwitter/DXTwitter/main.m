499664049543dfe854c407f34e1e71ef54b46b14
dx <dx@MiracleDebrisdeMacBook-Pro.local>
2017-03-11 14:40:12 +0800

    Add YYKit

---
+++ DXTwitter/DXTwitter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DXTwitter
+//
+//  Created by dx on 17/3/11.
+//  Copyright © 2017年 dx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
